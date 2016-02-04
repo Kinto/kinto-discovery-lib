2be51b5300673dfa0451c3cfc39eabeb0e8059fa
Alexis Métaireau <alexis@notmyidea.org>
2016-02-04 12:14:09 +0100

    Add tests for getUserIDHash

--- tests/discovery_test.js
+++ tests/discovery_test.js
@@ -4,13 +4,26 @@ import chai, { expect } from "chai";
 import chaiAsPromised from "chai-as-promised";
 import sinon from "sinon";
 
+import {getUserIDHash} from "../lib/index.js";
+
 chai.use(chaiAsPromised);
 chai.should();
 chai.config.includeStack = true;
 
+
 describe("getUserIDHash", () => {
   it("should return the same hash for the same userid", () => {
-    const hash1 = getUserIDHash('shweta');
-    expect(hash1).to.eql(getUserIDHash('shweta'));
+    const hash = getUserIDHash("shweta");
+    expect(hash).to.eql(getUserIDHash("shweta"));
+  });
+
+  it("should return different hashes for different values", () => {
+    const hash = getUserIDHash("shweta");
+    expect(hash).to.not.eql(getUserIDHash("alexis"));
+  });
+
+  it("should return an uuid", () => {
+    const hash = getUserIDHash("shweta");
+    expect(hash).to.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
   });
 });
