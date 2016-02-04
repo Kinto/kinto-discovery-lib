138b9891029ad887f95005516ce1d98122eb3ca2
Alexis Métaireau <alexis@notmyidea.org>
2016-02-04 13:28:54 +0100

    Start working on the registerUserURL function

--- tests/discovery_test.js
+++ tests/discovery_test.js
@@ -4,26 +4,64 @@ import chai, { expect } from "chai";
 import chaiAsPromised from "chai-as-promised";
 import sinon from "sinon";
 
-import {getUserIDHash} from "../lib/index.js";
+import {getUserIDHash, registerUserURL} from "../lib/index.js";
+import { fakeServerResponse } from "./test_utils.js";
 
 chai.use(chaiAsPromised);
 chai.should();
 chai.config.includeStack = true;
 
+const root = typeof window === "object" ? window : global;
 
-describe("getUserIDHash", () => {
-  it("should return the same hash for the same userid", () => {
-    const hash = getUserIDHash("shweta");
-    expect(hash).to.eql(getUserIDHash("shweta"));
+describe("discovery library", () => {
+  let sandbox;
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
   });
 
-  it("should return different hashes for different values", () => {
-    const hash = getUserIDHash("shweta");
-    expect(hash).to.not.eql(getUserIDHash("alexis"));
+  afterEach(() => {
+    sandbox.restore();
   });
 
-  it("should return an uuid", () => {
-    const hash = getUserIDHash("shweta");
-    expect(hash).to.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
+  describe("getUserIDHash", () => {
+    it("should return the same hash for the same userid", () => {
+      const hash = getUserIDHash("shweta");
+      expect(hash).to.eql(getUserIDHash("shweta"));
+    });
+
+    it("should return different hashes for different values", () => {
+      const hash = getUserIDHash("shweta");
+      expect(hash).to.not.eql(getUserIDHash("alexis"));
+    });
+
+    it("should return an uuid", () => {
+      const hash = getUserIDHash("shweta");
+      expect(hash).to.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
+    });
+  });
+
+  describe("registerUserURL", () => {
+
+    const centralRepositoryURL = "http://central.kinto-storage.com/v1";
+    const headers = {}
+    let userStorageURL;
+
+    describe("With an already existing user URL", () => {
+      userStorageURL = "https://my-kinto-instance.com/v1";
+      beforeEach(() => {
+        sandbox.stub(root, "fetch").returns(fakeServerResponse(200, {}, {
+          data: {url: userStorageURL}
+        }));
+      });
+
+      it("should return the existing URL", (done) => {
+        registerUserURL("userID", centralRepositoryURL, headers, userStorageURL)
+        .then((userURL) => {
+          console.log("we've got", userURL);
+          expect(userURL).to.eql("");
+          done();
+        });
+      });
+    });
   });
 });
